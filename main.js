007813b8414df4d757a2a5edd2ba37ed41648b35
lana del rey <129308917+lewishamilton123@users.noreply.github.com>
2023-11-01 08:14:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,25 @@ function tocaSomPom(){
     document.querySelector('# som_tecla_pom').play('');
 }
 
-document.querySelectorAll('.tecla')
+document.querySelectorAll('.tecla');
 
-const listasDeTeclas= document.querySelectorAll('.tecla')
+const listasDeTeclas= document.querySelectorAll('.tecla');
+
+listasDeTeclas[0].onclick= tocaSomPom;
+
+listasDeTeclas[1].onclick= tocaSomClap;
+listasDeTeclas[2].onclick= tocaSomTim;
+listasDeTeclas[3].onclick= tocaSomPuff;
+
+listasDeTeclas.onclick= function(){
+    tocaSom(isAudio);
+};
+
+tecla.onkeydown= function (){
+    if(Eventocode==='Space'){
+
+    tecla.classList.add('ativa');
+}
+tecla.onkeyup= function(){
+    tecla.classList.remove('ativa');
+}
